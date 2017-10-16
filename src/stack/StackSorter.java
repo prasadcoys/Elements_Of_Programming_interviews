b905429634de0f01c6b8bb31ff947dbc92e78cff
VARADHARAJAN <svaradharajan768@gmail.com>
2017-10-16 21:26:04 +0530

    committing change for sorting a two member stack

--- src/stack/StackSorter.java
+++ src/stack/StackSorter.java
@@ -5,7 +5,14 @@ import java.util.Stack;
 public class StackSorter {
     public static void sort(Stack<Integer> stack) {
 
-        Integer top = stack.peek();
+        Integer top = stack.pop();
+        if(top < stack.peek())
+        {
+            Integer temp = stack.pop();
+            stack.push(top);
+            stack.push(temp);
+        }
+
 
     }
 }
