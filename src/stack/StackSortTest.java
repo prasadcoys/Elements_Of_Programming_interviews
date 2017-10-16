6f8e6a38b94259e566fd21338705f0e92d0e897e
VARADHARAJAN <svaradharajan768@gmail.com>
2017-10-16 21:20:40 +0530

    Adding StackSorter

--- src/stack/StackSortTest.java
+++ src/stack/StackSortTest.java
@@ -1,5 +1,23 @@
 package stack;
 
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
 public class StackSortTest {
-    
+
+    @Test
+    public void testIfStackWithTwoElementsIsSortedInDescendingOrder(){
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(1);
+
+        StackSorter.sort(stack);
+        assertEquals(new Integer(2),stack.peek());
+
+
+    }
 }
---
+++ src/stack/StackSorter.java
@@ -0,0 +1,11 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackSorter {
+    public static void sort(Stack<Integer> stack) {
+
+        Integer top = stack.peek();
+
+    }
+}
