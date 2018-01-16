de7d2536a32cd234be7f447ab4194a60ae28508e
VARADHARAJAN <svaradharajan768@gmail.com>
2018-01-16 08:04:39 +0530

    min heap insert commit

--- src/C_0/utils/MinHeap.java
+++ src/C_0/utils/MinHeap.java
@@ -13,21 +13,57 @@ public class MinHeap
 
     public int getLeftChild(int i)
     {
-        return list.get((i*2)+1);
+        return list.get((i * 2) + 1);
     }
 
     public int getRightChild(int i)
     {
-        return list.get((i*2)+2);
+        return list.get((i * 2) + 2);
     }
 
     public boolean hasLeftChild(int i)
     {
-        return (((i*2)+1)<list.size());
+        return (((i * 2) + 1) < list.size());
     }
 
     public boolean hasRightChild(int i)
     {
-        return (((i*2)+2)<list.size());
+        return (((i * 2) + 2) < list.size());
+    }
+
+    public void insert(int i)
+    {
+        list.add(i);
+        int currentIndex = list.size() - 1;
+        int parentIndex = getParentIndex(currentIndex);
+        while (parentIndex >= 0)
+        {
+            Integer parentValue = list.get(parentIndex);
+            if (i < parentValue)
+            {
+                list.set(parentIndex, i);
+                list.set(currentIndex, parentValue);
+                currentIndex = parentIndex;
+                parentIndex = getParentIndex(currentIndex);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    public int getParentIndex(int currentIndex)
+    {
+        int parentIndex = 0;
+        if (currentIndex % 2 == 0)
+        {
+            parentIndex = ((currentIndex) / 2) - 1;
+        } else
+        {
+            parentIndex = (currentIndex) / 2;
+
+        }
+        return parentIndex;
     }
 }
--- src/C_0/utils/MinHeapTest.java
+++ src/C_0/utils/MinHeapTest.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class MinHeapTest
 {
@@ -34,5 +32,45 @@ public class MinHeapTest
         assertTrue(heap.hasLeftChild(0));
         assertFalse(heap.hasRightChild(1));
         assertTrue(heap.hasRightChild(0));
+    }
+
+    @Test
+    public void testIfInsertOfGreaterElementWorks()
+    {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        MinHeap heap = new MinHeap(list);
+        heap.insert(4);
+        assertTrue(heap.hasLeftChild(1));
+        assertEquals(4, heap.getLeftChild(1));
+    }
+
+    @Test
+    public void testIfInsertOfLesserElementWorks()
+    {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        MinHeap heap = new MinHeap(list);
+        heap.insert(2);
+        assertTrue(heap.hasLeftChild(1));
+        assertEquals(3, heap.getLeftChild(1));
+    }
+    @Test
+    public void testIfInsertOfLesserElementToRightChildWorks()
+    {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        MinHeap heap = new MinHeap(list);
+        heap.insert(2);
+        assertTrue(heap.hasRightChild(1));
+        assertEquals(5, heap.getLeftChild(1));
+        assertEquals(3, heap.getRightChild(1));
     }
 }
