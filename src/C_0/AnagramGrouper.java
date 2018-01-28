e9a7902d883ae5eb19177e13c3233c65ffc9d033
VARADHARAJAN <svaradharajan768@gmail.com>
2018-01-28 20:21:21 +0530

    anagram groups my solution

--- src/C_0/AnagramGrouper.java
+++ src/C_0/AnagramGrouper.java
@@ -1,18 +1,43 @@
 package C_0;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import C_0.utils.AnagramTester;
+
+import java.util.*;
 
 public class AnagramGrouper
 {
     public static List<List<String>> getGroupsOfAnagrams(List<String> listOfWords)
     {
-        Map<String,List<String>> wordGrouping = new HashMap<>();
-        for(String word : listOfWords)
+        Map<String, List<String>> wordGrouping = new HashMap<>();
+        List<List<String>> anagramGroups = new ArrayList<>();
+        for (String word : listOfWords)
         {
+            int[] wordsInIntArray = AnagramTester
+                    .getIntegerArrayFromWord(word);
+            String wordForKeyInMap = null;
+            for (int i = 0; i < wordsInIntArray.length; i++)
+            {
+                wordForKeyInMap += (wordsInIntArray[ i ] + '*');
+                wordForKeyInMap += i + ',';
+            }
+            if (wordGrouping.containsKey(wordForKeyInMap))
+            {
+                wordGrouping.get(wordForKeyInMap).add(word);
+            } else
+            {
+                ArrayList<String> strings = new ArrayList<>();
+                strings.add(word);
+                wordGrouping.put(wordForKeyInMap, strings);
+            }
 
         }
-        return null;
+        for(Map.Entry<String,List<String>> entry: wordGrouping.entrySet())
+        {
+            if(entry.getValue().size()>1)
+            {
+                anagramGroups.add(entry.getValue());
+            }
+        }
+        return anagramGroups;
     }
 }
