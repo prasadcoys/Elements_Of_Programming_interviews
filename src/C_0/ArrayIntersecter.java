da9132028add38ecc6617ed1e8541c6d3af6e77e
VARADHARAJAN <svaradharajan768@gmail.com>
2018-01-29 18:55:31 +0530

    My solution for array intersection

--- src/C_0/ArrayIntersecter.java
+++ src/C_0/ArrayIntersecter.java
@@ -1,9 +1,45 @@
 package C_0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayIntersecter
 {
-    public static int[] intersect(int[] a, int[] b)
+    public static List<Integer> intersect(int[] a, int[] b)
     {
-        return new int[ 0 ];
+        List<Integer> listWithIntersectionElements = new ArrayList<>();
+        int i = 0, j = 0;
+        while (true)
+        {
+            if (i >= a.length || j >= b.length)
+            {
+                break;
+            }
+            if (a[ i ] == b[ j ])
+            {
+                if (!listWithIntersectionElements
+                        .isEmpty() && (listWithIntersectionElements.get
+                        (listWithIntersectionElements.size() - 1) ==
+                        a[ i ]))
+                {
+                    i++;
+                    j++;
+                    continue;
+                }
+                listWithIntersectionElements.add(a[ i ]);
+                i++;
+                j++;
+            }
+            else if(a[i]<b[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+
+        }
+        return listWithIntersectionElements;
     }
 }
--- src/C_0/IntersectionOfTwoSortedArraysTest.java
+++ src/C_0/IntersectionOfTwoSortedArraysTest.java
@@ -2,7 +2,11 @@ package C_0;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class IntersectionOfTwoSortedArraysTest
 {
@@ -11,7 +15,58 @@ public class IntersectionOfTwoSortedArraysTest
     {
         int[] a = {2,3,5,5,6,7,7,8,12};
         int[] b = {5,5,6,8,8,9,10,10};
-        int[] expected = { 5,6,8};
+        List<Integer> expectedList = new ArrayList<>();
+        expectedList.add(5);
+        expectedList.add(6);
+        expectedList.add(8);
+        assertEquals(expectedList,ArrayIntersecter.intersect(a,b));
+    }
+
+    @Test
+    public void testIfIntersectionOfTwoDisjointSetsGivesCorrectResults()
+    {
+        int[] a = {1,2,3,4};
+        int[] b = {5,6,7,8};
+        assertTrue(ArrayIntersecter.intersect(a,b).isEmpty());
+    }
+
+    @Test
+    public void testIfIntersectionOfTwoEqualSetsGivesTheSameSet()
+    {
+        int[] a = {1,2,3,4};
+        int[] b = {1,2,3,4};
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
         assertEquals(expected,ArrayIntersecter.intersect(a,b));
     }
+    @Test
+    public void testIfInteserctionOfTwoSingletonsGivesASingleton()
+    {
+        int[] a = {1};
+        int[] b = {1};
+        List<Integer> expected  = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected,ArrayIntersecter.intersect(a,b));
+    }
+    @Test
+    public void testIfIntersectionBetweenTwoEmptySetsGivesEmptyResult()
+    {
+        int[] a = {};
+        int[] b = {};
+        assertTrue(ArrayIntersecter.intersect(a,b).isEmpty());
+    }
+    @Test
+    public void
+            testIfIntersectionBetweenTwoSetsThatHaveMultipleInstancesOfSameNumberGivesResultOfOnlyThatNumber()
+    {
+        int[] a= {1,1,1,1,1};
+        int[] b = {1,1,1};
+        List<Integer> expected  = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected,ArrayIntersecter.intersect(a,b));
+    }
+
 }
