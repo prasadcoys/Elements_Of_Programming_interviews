e9b5815f9fb7a7dd7409c5d84dc1489138a969fe
VARADHARAJAN <svaradharajan768@gmail.com>
2018-02-15 20:30:45 +0530

    memoized recursion for rod cutting problem

--- src/C_0/dynamic/programming/RodCuttingCracker.java
+++ src/C_0/dynamic/programming/RodCuttingCracker.java
@@ -1,21 +1,53 @@
 package C_0.dynamic.programming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RodCuttingCracker
 {
-    public static int getMaximumRevenue(int length, int[] prices)
+    static Map<Integer, Integer> pricesMap = new HashMap<>();
+
+    public static int getMaximumRevenueNaiveRecursion(int length, int[] prices)
     {
         int i = 1;
         int currentMax = -10000;
-        if(length==1)
+        if (length == 1)
         {
-            return prices[0];
+            return prices[ 0 ];
         }
-        while(i<length)
+        while (i < length)
         {
-             currentMax = Math.max(currentMax,Math.max(prices[length-1],
-                     getMaximumRevenue(i,
-                     prices)+getMaximumRevenue(length-i,prices)));
-             i++;
+            currentMax = Math.max(currentMax, Math.max(prices[ length - 1 ],
+                    getMaximumRevenueNaiveRecursion(i,
+                            prices) + getMaximumRevenueNaiveRecursion(length - i, prices)));
+            i++;
+        }
+
+        return currentMax;
+    }
+
+    public static int getMaximumRevenueMemoizedRecursion(int length, int[] prices)
+    {
+        int i = 1;
+        int currentMax = -10000;
+        if(pricesMap.containsKey(length))
+        {
+            System.out.println("length already calcuated "+length);
+            return pricesMap.get(length);
+        }
+        System.out.println("length not already present "+length);
+        if (length == 1)
+        {
+            pricesMap.put(length,prices[length-1]);
+            return prices[ 0 ];
+        }
+        while (i < length)
+        {
+            currentMax = Math.max(currentMax, Math.max(prices[ length - 1 ],
+                    getMaximumRevenueMemoizedRecursion(i,
+                            prices) + getMaximumRevenueMemoizedRecursion
+                            (length - i, prices)));
+            i++;
         }
 
         return currentMax;
--- src/C_0/dynamic/programming/RodCuttingProblemTest.java
+++ src/C_0/dynamic/programming/RodCuttingProblemTest.java
@@ -1,6 +1,5 @@
 package C_0.dynamic.programming;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,35 +7,69 @@ import static org.junit.Assert.assertEquals;
 public class RodCuttingProblemTest
 {
     @Test
-    public void testIfRodCuttingProblemCrackerWorksCorrectly()
+    public void
+    testIfRodCuttingProblemCrackerWorksCorrectlyByNaiveRecursion()
     {
-        int[] prices = {1,5,8,9,10,17,17,20,24,30};
-        assertEquals(1,RodCuttingCracker.getMaximumRevenue(1,prices));
-        assertEquals(10,RodCuttingCracker.getMaximumRevenue(4,prices));
-        assertEquals(13,RodCuttingCracker.getMaximumRevenue(5,prices));
-        assertEquals(22,RodCuttingCracker.getMaximumRevenue(8,prices));
-        assertEquals(30,RodCuttingCracker.getMaximumRevenue(10,prices));
+        int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueNaiveRecursion(1, prices));
+        assertEquals(10, RodCuttingCracker.getMaximumRevenueNaiveRecursion(4, prices));
+        assertEquals(13, RodCuttingCracker.getMaximumRevenueNaiveRecursion(5, prices));
+        assertEquals(22, RodCuttingCracker.getMaximumRevenueNaiveRecursion(8, prices));
+        assertEquals(30, RodCuttingCracker.getMaximumRevenueNaiveRecursion(10, prices));
     }
 
     @Test
     public void
-            testIfRodCuttingForPricesWhereNoCutYieldsBestPriceIsComputedCorrectly()
+    testIfRodCuttingForPricesWhereNoCutYieldsBestPriceIsComputedCorrectlyByNaiveRecursion()
     {
-        int[] prices = {1,2,3,4};
-        assertEquals(1,RodCuttingCracker.getMaximumRevenue(1,prices));
-        assertEquals(2,RodCuttingCracker.getMaximumRevenue(2,prices));
-        assertEquals(3,RodCuttingCracker.getMaximumRevenue(3,prices));
-        assertEquals(4,RodCuttingCracker.getMaximumRevenue(4,prices));
+        int[] prices = {1, 2, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueNaiveRecursion(1, prices));
+        assertEquals(2, RodCuttingCracker.getMaximumRevenueNaiveRecursion(2, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueNaiveRecursion(3, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueNaiveRecursion(4, prices));
     }
 
     @Test
     public void
-    testIfRodCuttingForPricesWhereOneCutYieldsBestPriceIsComputedCorrectly()
+    testIfRodCuttingForPricesWhereOneCutYieldsBestPriceIsComputedCorrectlyByNaiveRecursion()
     {
-        int[] prices = {1,3,3,4};
-        assertEquals(1,RodCuttingCracker.getMaximumRevenue(1,prices));
-        assertEquals(3,RodCuttingCracker.getMaximumRevenue(2,prices));
-        assertEquals(4,RodCuttingCracker.getMaximumRevenue(3,prices));
+        int[] prices = {1, 3, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueNaiveRecursion(1, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueNaiveRecursion(2, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueNaiveRecursion(3, prices));
+    }
+    @Test
+    public void
+    testIfRodCuttingProblemCrackerWorksCorrectlyByMemoizedRecursion()
+    {
+        int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        assertEquals(1, RodCuttingCracker
+                .getMaximumRevenueMemoizedRecursion(1, prices));
+        assertEquals(10, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(4, prices));
+        assertEquals(13, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(5, prices));
+        assertEquals(22, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(8, prices));
+        assertEquals(30, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(10, prices));
+    }
+
+    @Test
+    public void
+    testIfRodCuttingForPricesWhereNoCutYieldsBestPriceIsComputedCorrectlyByMemoizedRecursion()
+    {
+        int[] prices = {1, 2, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(1, prices));
+        assertEquals(2, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(2, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(3, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(4, prices));
+    }
+
+    @Test
+    public void
+    testIfRodCuttingForPricesWhereOneCutYieldsBestPriceIsComputedCorrectlyByMemoizedRecursion()
+    {
+        int[] prices = {1, 3, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(1, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(2, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(3, prices));
     }
 
 }
