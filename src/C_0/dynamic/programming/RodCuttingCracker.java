e6bdd9d6a4b78ad7fa8d79caa7cadf18dfcdf196
VARADHARAJAN <svaradharajan768@gmail.com>
2018-02-18 21:48:39 +0530

    rod cutting - bottom up approach - partial solution

--- src/C_0/dynamic/programming/RodCuttingCracker.java
+++ src/C_0/dynamic/programming/RodCuttingCracker.java
@@ -7,6 +7,11 @@ public class RodCuttingCracker
 {
     static Map<Integer, Integer> pricesMap = new HashMap<>();
 
+    public static void init()
+    {
+        pricesMap = new HashMap<>();
+    }
+
     public static int getMaximumRevenueNaiveRecursion(int length, int[] prices)
     {
         int i = 1;
@@ -30,15 +35,13 @@ public class RodCuttingCracker
     {
         int i = 1;
         int currentMax = -10000;
-        if(pricesMap.containsKey(length))
+        if (pricesMap.containsKey(length))
         {
-            System.out.println("length already calcuated "+length);
             return pricesMap.get(length);
         }
-        System.out.println("length not already present "+length);
         if (length == 1)
         {
-            pricesMap.put(length,prices[length-1]);
+            pricesMap.put(length, prices[ length - 1 ]);
             return prices[ 0 ];
         }
         while (i < length)
@@ -49,7 +52,42 @@ public class RodCuttingCracker
                             (length - i, prices)));
             i++;
         }
-        pricesMap.put(length,currentMax);
+        pricesMap.put(length, currentMax);
+        return currentMax;
+    }
+
+    public static int getMaximumRevenueBottomUp(int length, int[] prices)
+    {
+        int[] maximumRevenues = new int[ prices.length ];
+        int i = 0;
+        while (i < length)
+        {
+            if(i == 0)
+            {
+                maximumRevenues[0] = prices[0];
+                i++;
+            }
+            else
+            {
+                maximumRevenues[i] = Math.max(prices[i],getMaxRevenue(i,
+                        prices,maximumRevenues));
+                i++;
+            }
+        }
+        return maximumRevenues[length-1];
+    }
+
+    private static int getMaxRevenue(int i, int[] prices, int[] maximumRevenues)
+    {
+        int currentMax = prices[i];
+        int j = 1;
+        while(i>=j)
+        {
+            currentMax = Math.max(currentMax,
+                    (maximumRevenues[i-j]+
+                            maximumRevenues[j]));
+            j++;
+        }
         return currentMax;
     }
 }
--- src/C_0/dynamic/programming/RodCuttingProblemTest.java
+++ src/C_0/dynamic/programming/RodCuttingProblemTest.java
@@ -43,6 +43,7 @@ public class RodCuttingProblemTest
     testIfRodCuttingProblemCrackerWorksCorrectlyByMemoizedRecursion()
     {
         int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        RodCuttingCracker.init();
         assertEquals(1, RodCuttingCracker
                 .getMaximumRevenueMemoizedRecursion(1, prices));
         assertEquals(10, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(4, prices));
@@ -56,6 +57,7 @@ public class RodCuttingProblemTest
     testIfRodCuttingForPricesWhereNoCutYieldsBestPriceIsComputedCorrectlyByMemoizedRecursion()
     {
         int[] prices = {1, 2, 3, 4};
+        RodCuttingCracker.init();
         assertEquals(1, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(1, prices));
         assertEquals(2, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(2, prices));
         assertEquals(3, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(3, prices));
@@ -67,6 +69,7 @@ public class RodCuttingProblemTest
     testIfRodCuttingForPricesWhereOneCutYieldsBestPriceIsComputedCorrectlyByMemoizedRecursion()
     {
         int[] prices = {1, 3, 3, 4};
+        RodCuttingCracker.init();
         assertEquals(1, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(1, prices));
         assertEquals(3, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(2, prices));
         assertEquals(4, RodCuttingCracker.getMaximumRevenueMemoizedRecursion(3, prices));
@@ -78,7 +81,53 @@ public class RodCuttingProblemTest
         int[] prices = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
                 20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,
                 38,39,40};
+        RodCuttingCracker.init();
         assertEquals(40,RodCuttingCracker
                 .getMaximumRevenueMemoizedRecursion(40,prices));
+    }
+
+    @Test
+    public void
+    testIfRodCuttingProblemCrackerWorksCorrectlyByBottomUpLogic()
+    {
+        int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        assertEquals(1, RodCuttingCracker
+                .getMaximumRevenueMemoizedRecursion(1, prices));
+        assertEquals(10, RodCuttingCracker.getMaximumRevenueBottomUp(4,
+                prices));
+        assertEquals(13, RodCuttingCracker.getMaximumRevenueBottomUp(5, prices));
+        assertEquals(22, RodCuttingCracker.getMaximumRevenueBottomUp(8, prices));
+        assertEquals(30, RodCuttingCracker.getMaximumRevenueBottomUp(10, prices));
+    }
+
+    @Test
+    public void
+    testIfRodCuttingForPricesWhereNoCutYieldsBestPriceIsComputedCorrectlyByBottomUpLogic()
+    {
+        int[] prices = {1, 2, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueBottomUp(1, prices));
+        assertEquals(2, RodCuttingCracker.getMaximumRevenueBottomUp(2, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueBottomUp(3, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueBottomUp(4, prices));
+    }
+
+    @Test
+    public void
+    testIfRodCuttingForPricesWhereOneCutYieldsBestPriceIsComputedCorrectlyByBottomUpLogic()
+    {
+        int[] prices = {1, 3, 3, 4};
+        assertEquals(1, RodCuttingCracker.getMaximumRevenueBottomUp(1, prices));
+        assertEquals(3, RodCuttingCracker.getMaximumRevenueBottomUp(2, prices));
+        assertEquals(4, RodCuttingCracker.getMaximumRevenueBottomUp(3, prices));
+    }
+
+    @Test
+    public void testAVeryLargeInputSizeByBottomUpLogic()
+    {
+        int[] prices = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,
+                20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,
+                38,39,40};
+        assertEquals(40,RodCuttingCracker
+                .getMaximumRevenueBottomUp(40,prices));
     }
 }
