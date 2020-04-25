ac96cb7af33fea2b3d193b32d42d19c830ccdaf2
VARADHARAJAN <svaradharajan768@gmail.com>
2020-04-25 13:00:21 +0530

    subsets solution

--- src/backtracking/Subsets.java
+++ src/backtracking/Subsets.java
@@ -1,4 +1,29 @@
 package backtracking;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        recursivelyAddSubsetToSubsets(subsets,nums,
+                new ArrayList<Integer>(),0);
+        return subsets;
+    }
+
+    private void recursivelyAddSubsetToSubsets(List<List<Integer>> subsets, int[] nums, List<Integer> currentSubset,int start) {
+        if (nums.length == start){
+            subsets.add(currentSubset);
+            return;
+        }
+        recursivelyAddSubsetToSubsets(subsets,nums,currentSubset,start+1);
+        List<Integer> subsetWithElement = new ArrayList<>(currentSubset);
+        subsetWithElement.add(nums[start]);
+        recursivelyAddSubsetToSubsets(subsets,
+                nums,
+        subsetWithElement,start+1);
+    }
+
 }
--- src/backtracking/SubsetsTest.java
+++ src/backtracking/SubsetsTest.java
@@ -1,4 +1,56 @@
 package backtracking;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubsetsTest {
+
+
+    @Test
+    public void testIfSubsetsForAnArrayInSortedArrayWorks() {
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{1, 2, 3}, {1}, {2}, {3}, {1, 2}
+                , {1, 3}, {2, 3}, {}};
+        addArrayToList(expectedSubsets, expectedArray);
+
+        List<List<Integer>> subsets = new Subsets().subsets(new int[]{1, 2, 3});
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+
+    }
+
+    @Test
+    public void testIfSubsetsForAnArrayInNonSortedArrayWorks() {
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{}, {9}, {0}, {0, 9}, {3}, {3, 9}, {0, 3}, {0, 3, 9}, {5}, {5, 9}, {0, 5}, {0, 5, 9}, {3, 5}, {3, 5, 9}, {0, 3, 5}, {0, 3, 5, 9}, {7}, {7, 9}, {0, 7}, {0, 7, 9}, {3, 7}, {3, 7, 9}, {0, 3, 7}, {0, 3, 7, 9}, {5, 7}, {5, 7, 9}, {0, 5, 7}, {0, 5, 7, 9}, {3, 5, 7}, {3, 5, 7, 9}, {0, 3, 5, 7}, {0, 3, 5, 7, 9}};
+        addArrayToList(expectedSubsets, expectedArray);
+        int[] nums = new int[]{9,0,3,5,7};
+        Arrays.sort(nums);
+        List<List<Integer>> subsets =
+                new Subsets().subsets(nums);
+        subsets.removeAll(expectedSubsets);
+        System.out.println(subsets);
+        Assert.assertTrue(subsets.isEmpty());
+
+
+    }
+
+
+    private void addArrayToList(List<List<Integer>> expectedSubsets, int[][] expectedArray) {
+        for (int[] array :
+                expectedArray) {
+            List<Integer> list = new ArrayList<>();
+            for (int num :
+                    array) {
+                list.add(num);
+            }
+            expectedSubsets.add(list);
+        }
+    }
+
+
 }
