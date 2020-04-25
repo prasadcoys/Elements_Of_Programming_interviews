21d3948e4995d442aa55a60671d0c0750cb108b9
VARADHARAJAN <svaradharajan768@gmail.com>
2020-04-25 23:10:24 +0530

    efficient solution for subsets 2 problem

--- src/backtracking/Subsets.java
+++ src/backtracking/Subsets.java
@@ -1,29 +1,125 @@
 package backtracking;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> subsets = new ArrayList<>();
-        recursivelyAddSubsetToSubsets(subsets,nums,
-                new ArrayList<Integer>(),0);
+        Arrays.sort(nums);
+        recursivelyAddSubsetToSubsets(subsets, nums,
+                new ArrayList<>(), 0);
         return subsets;
     }
 
-    private void recursivelyAddSubsetToSubsets(List<List<Integer>> subsets, int[] nums, List<Integer> currentSubset,int start) {
-        if (nums.length == start){
+    private void recursivelyAddSubsetToSubsets(List<List<Integer>> subsets, int[] nums, List<Integer> currentSubset, int start) {
+        if (nums.length == start) {
             subsets.add(currentSubset);
             return;
         }
-        recursivelyAddSubsetToSubsets(subsets,nums,currentSubset,start+1);
+        recursivelyAddSubsetToSubsets(subsets, nums, currentSubset, start + 1);
         List<Integer> subsetWithElement = new ArrayList<>(currentSubset);
-        subsetWithElement.add(nums[start]);
+        subsetWithElement.add(nums[ start ]);
         recursivelyAddSubsetToSubsets(subsets,
                 nums,
-        subsetWithElement,start+1);
+                subsetWithElement, start + 1);
     }
 
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        Arrays.sort(nums);
+        Map<String, Boolean> hashcodes = new HashMap<>();
+        recursivelyAddNonDuplicateSubsetToSubsets(subsets, nums,
+                new ArrayList<>(), 0, hashcodes);
+        return subsets;
+    }
+
+    private void recursivelyAddNonDuplicateSubsetToSubsets(
+            List<List<Integer>> subsets,
+            int[] nums,
+            List<Integer> currentSubset,
+            int start, Map<String, Boolean> hashcodes) {
+        if (nums.length == start) {
+            String hashCode = calculateHashCode(currentSubset);
+            if (hashcodes.containsKey(hashCode)) {
+                return;
+            }
+            subsets.add(currentSubset);
+            hashcodes.put(hashCode, true);
+            return;
+        }
+        recursivelyAddNonDuplicateSubsetToSubsets(subsets, nums,
+                currentSubset, start + 1, hashcodes);
+        List<Integer> subsetWithElement = new ArrayList<>(currentSubset);
+        subsetWithElement.add(nums[ start ]);
+        recursivelyAddNonDuplicateSubsetToSubsets(subsets,
+                nums,
+                subsetWithElement, start + 1, hashcodes);
+    }
+
+    private String calculateHashCode(List<Integer> currentSubset) {
+        String code = "";
+        for (int num :
+                currentSubset) {
+            code += (num);
+        }
+        System.out.println(code);
+        return code;
+
+    }
+
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        Arrays.sort(nums);
+        recursivelyAddNonDuplicateSubsetToSubsets2(subsets, nums,
+                new ArrayList<>(), 0, true);
+        return subsets;
+    }
+
+    private void recursivelyAddNonDuplicateSubsetToSubsets2(
+            List<List<Integer>> subsets,
+            int[] nums,
+            List<Integer> currentSubset,
+            int start, boolean isTrueTree) {
+
+        if (nums.length <= start) {
+            System.out.println("start :" +start+currentSubset);
+            subsets.add(currentSubset);
+            return;
+        }
+
+        int i = start;
+        if (!isTrueTree) {
+            while (i < nums.length) {
+                if (nums[ i - 1 ] == nums[ i ]) {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+        }
+        if (isTrueTree) {
+            recursivelyAddNonDuplicateSubsetToSubsets2(subsets, nums,
+                    currentSubset, start + 1, false);
+            List<Integer> subsetWithElement = new ArrayList<>(currentSubset);
+            subsetWithElement.add(nums[ start ]);
+            recursivelyAddNonDuplicateSubsetToSubsets2(subsets,
+                    nums,
+                    subsetWithElement, start + 1, true);
+        }
+        else if ( i <nums.length){
+            recursivelyAddNonDuplicateSubsetToSubsets2(subsets, nums,
+                    currentSubset, i + 1, false);
+            List<Integer> subsetWithElement = new ArrayList<>(currentSubset);
+            subsetWithElement.add(nums[i]);
+            recursivelyAddNonDuplicateSubsetToSubsets2(subsets,
+                    nums,
+                    subsetWithElement, i + 1, true);
+        }
+        else {
+            subsets.add(currentSubset);
+        }
+    }
 }
+
+
+
--- src/backtracking/SubsetsTest.java
+++ src/backtracking/SubsetsTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class SubsetsTest {
@@ -29,17 +28,84 @@ public class SubsetsTest {
         int[][] expectedArray = new int[][]{{}, {9}, {0}, {0, 9}, {3}, {3, 9}, {0, 3}, {0, 3, 9}, {5}, {5, 9}, {0, 5}, {0, 5, 9}, {3, 5}, {3, 5, 9}, {0, 3, 5}, {0, 3, 5, 9}, {7}, {7, 9}, {0, 7}, {0, 7, 9}, {3, 7}, {3, 7, 9}, {0, 3, 7}, {0, 3, 7, 9}, {5, 7}, {5, 7, 9}, {0, 5, 7}, {0, 5, 7, 9}, {3, 5, 7}, {3, 5, 7, 9}, {0, 3, 5, 7}, {0, 3, 5, 7, 9}};
         addArrayToList(expectedSubsets, expectedArray);
         int[] nums = new int[]{9,0,3,5,7};
-        Arrays.sort(nums);
         List<List<Integer>> subsets =
                 new Subsets().subsets(nums);
         subsets.removeAll(expectedSubsets);
-        System.out.println(subsets);
+        System.out.println(expectedSubsets);
         Assert.assertTrue(subsets.isEmpty());
-
-
     }
 
+    @Test
+    public void testIfSubsetsWithoutDuplicatesAreGeneratedIfArrayContainsduplicates(){
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{1},{2},{1,2},{2,2},
+                {1,2,2}, {}};
+        addArrayToList(expectedSubsets, expectedArray);
 
+        List<List<Integer>> subsets =
+                new Subsets().subsetsWithDup2(new int[]{1, 2, 2});
+        System.out.println(subsets);
+        Assert.assertEquals(expectedSubsets.size(),subsets.size());
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+    }
+
+    @Test
+    public void testIfSubsetsWithoutDuplicatesAreGeneratedIfArrayContainsOnlyduplicates(){
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{2},{2,2},{2,2,2}, {}};
+        addArrayToList(expectedSubsets, expectedArray);
+
+        List<List<Integer>> subsets =
+                new Subsets().subsetsWithDup2(new int[]{2, 2, 2});
+        System.out.println(subsets);
+        Assert.assertEquals(expectedSubsets.size(),subsets.size());
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+    }
+
+    @Test
+    public void testIfSubsetsWithoutDuplicatesAreGeneratedIfArrayContainsduplicatesFor0(){
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{0}, {}};
+        addArrayToList(expectedSubsets, expectedArray);
+
+        List<List<Integer>> subsets =
+                new Subsets().subsetsWithDup(new int[]{0});
+        System.out.println(subsets);
+        Assert.assertEquals(expectedSubsets.size(),subsets.size());
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+    }
+
+    @Test
+    public void testIfSubsetsWithoutDuplicatesAreGeneratedIfArrayContainsNoDuplicatesFor0(){
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{0}, {},{1},{4},{4,1},{4,0},
+                {1,0},{4,1,0}};
+        addArrayToList(expectedSubsets, expectedArray);
+
+        List<List<Integer>> subsets =
+                new Subsets().subsetsWithDup(new int[]{4,1,0});
+        System.out.println(subsets);
+        Assert.assertEquals(expectedSubsets.size(),subsets.size());
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+    }
+
+    @Test
+    public void testIfSubsetsWithoutDuplicatesAreGeneratedIfArrayContainsNoDuplicatesForNegativeNumber(){
+        List<List<Integer>> expectedSubsets = new ArrayList<>();
+        int[][] expectedArray = new int[][]{{},{-1},{-1,1},{-1,1,2},{-1,
+                2},{1},{1,2},{2}};
+        addArrayToList(expectedSubsets, expectedArray);
+
+        List<List<Integer>> subsets =
+                new Subsets().subsetsWithDup(new int[]{-1,1,2});
+        Assert.assertEquals(expectedSubsets.size(),subsets.size());
+        subsets.removeAll(expectedSubsets);
+        Assert.assertTrue(subsets.isEmpty());
+    }
     private void addArrayToList(List<List<Integer>> expectedSubsets, int[][] expectedArray) {
         for (int[] array :
                 expectedArray) {
