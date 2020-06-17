187ea9e3f4c2b3367f82ddc223e21c1e3e8ab4bb
VARADHARAJAN <svaradharajan768@gmail.com>
2020-06-17 08:39:31 +0530

    largest divisible subset

--- src/dynamicprogramming/longestincreasingsubsequence/LargestDivisibleTest.java
+++ src/dynamicprogramming/longestincreasingsubsequence/LargestDivisibleTest.java
@@ -21,9 +21,9 @@ public class LargestDivisibleTest {
         result = dpTable.get(dpTable.size()-1);
 
         for (int i = 1; i < nums.length; i++) {
-            List<Integer> tempList = new ArrayList<>();
+
             for (int j = 0; j < i; j++) {
-                tempList = new ArrayList<>();
+                List<Integer> tempList = new ArrayList<>();
                 if (nums[i]% nums[j] == 0){
                     if(dpTable.get(j).size() >= tempList.size()) {
                         tempList.addAll(dpTable.get(j));
@@ -31,16 +31,16 @@ public class LargestDivisibleTest {
 
                     }
                 }
+                if(dpTable.get(i).size()<tempList.size()) {
+                    dpTable.set(i, tempList);
+                }
+                if(result.size()<tempList.size()){
+                    result = tempList;
 
+                }
 
             }
-            if(dpTable.get(i).size()<tempList.size()) {
-                dpTable.set(i, tempList);
-            }
-            if(result.size()<tempList.size()){
-                result = tempList;
 
-            }
         }
 
         return result;
